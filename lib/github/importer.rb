8fd2bbee5f46646117640a1542e367f3d0baa5cf
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-22 13:49:39 +0900

    Implement simple user importer

--- lib/github/importer.rb
+++ lib/github/importer.rb
@@ -1,8 +1,24 @@
+require 'octokit'
+
 module Github
   class Importer
     def import
-      tokens = Rails.application.secrets[:github_access_tokens]
-      tokens
+      access_tokens.each do |access_token|
+        client = Octokit::Client.new(access_token: access_token)
+        import_users(client)
+      end
+    end
+
+    private
+
+    def import_users(client)
+      users = client.all_users
+    rescue => e
+      puts "#{e.class}: #{e}"
+    end
+
+    def access_tokens
+      @access_tokens ||= Rails.application.secrets[:github_access_tokens]
     end
   end
 end

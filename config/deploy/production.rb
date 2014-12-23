35b36d80210b3b54ff6a1ab82ddbeb8e148c755d
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-23 20:58:13 +0900

    Add db.cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
 
-server ENV['DEPLOY_SSH_HOST'], user: ENV['DEPLOY_SSH_USER'], roles: %w{web app db}
+server ENV['DEPLOY_SSH_HOST'], user: ENV['DEPLOY_SSH_USER'], roles: %w[web app db]

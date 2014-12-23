16884302fabb5bc4525cd1f342f03db0708e924a
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-23 18:04:09 +0900

    Precompile fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 

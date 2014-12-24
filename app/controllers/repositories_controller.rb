94bc1c3601af145a8f69ad5a35ce0a8ed97d1a4e
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-24 13:22:06 +0900

    Easy way to fetch count, not properly

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,6 @@
 class RepositoriesController < ApplicationController
   def index
     @repos = Repository.first(20)
-    @repos_count = Rails.cache.fetch('repositories_count', expires_in: 1.hour) do
-      Repository.count
-    end
+    @repos_count = Repository.last.id
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   def index
     @users = User.first(30)
-    @users_count = Rails.cache.fetch('users_count', expires_in: 1.hour) do
-      User.count
-    end
+    @users_count = User.last.id
   end
 end

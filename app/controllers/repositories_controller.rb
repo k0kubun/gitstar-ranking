ad28beedd7847ac28ea22202fc6e9fa20faa0faa
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-24 13:19:03 +0900

    Cache records count

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,8 @@
 class RepositoriesController < ApplicationController
   def index
     @repos = Repository.first(20)
-    @repos_count = Repository.count
+    @repos_count = Rails.cache.fetch('repositories_count', expires_in: 1.hour) do
+      Repository.count
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.first(30)
-    @users_count = User.count
+    @users_count = Rails.cache.fetch('users_count', expires_in: 1.hour) do
+      User.count
+    end
   end
 end

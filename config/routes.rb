21dc99cb4e777dad35ff072ff80be8682694af6b
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-26 01:00:12 +0900

    Change subdomain constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  constraints subdomain: 'sidekiq' do
+  constraints subdomain: 'internal' do
     mount Sidekiq::Web, at: '/sidekiq'
   end
 

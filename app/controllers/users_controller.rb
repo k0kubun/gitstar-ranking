3d3f5b28aeb7de9aebc1783bd739ebed18a2b7da
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-25 12:43:50 +0900

    Show user rankings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.first(30)
+    @users = User.where(type: 'User').order(stargazers_count: :desc).first(30)
     @users_count = User.last.id
+  end
+
+  def orgs
+    @users = User.where(type: 'Organization').order(stargazers_count: :desc).first(50)
+    @users_count = User.last.id
+    render action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
+  resources :users, only: [] do
+    collection do
+      get :orgs
+    end
+  end
+
   resources :repositories, only: :index
 end

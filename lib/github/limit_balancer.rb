a521832e9a7bd2e2cbca5f599a1362cf39627247
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-26 22:18:35 +0900

    Fix fatal bug

--- lib/github/limit_balancer.rb
+++ lib/github/limit_balancer.rb
@@ -6,9 +6,7 @@ module Github
     include Singleton
 
     def client
-      selected = clients.min { |c| c.rate_limit.remaining }
-      logger.info(clients.map { |c| c.rate_limit.remaining }.to_s + ': ' + selected.rate_limit.remaining.to_s)
-      selected
+      clients.max_by { |c| c.rate_limit.remaining }
     end
 
     def log_limit

23c96d0d23d8c4b007082dcdc79b54cee92f6e18
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-27 03:33:15 +0900

    Fix fatal bug

--- lib/github/job_scheduler.rb
+++ lib/github/job_scheduler.rb
@@ -6,7 +6,7 @@ module Github
     def self.schedule
       user_ids = User.where(queued_at: nil).limit(SET_LENGTH * SET_COUNT).pluck(:id)
       user_ids.each_slice(SET_LENGTH) do |id_set|
-        UserUpdateJob.perform_later(id_set)
+        StarCountJob.perform_later(id_set)
       end
       User.where(id: user_ids).update_all(queued_at: Time.now)
     end

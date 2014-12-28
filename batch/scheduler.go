a3d00a73f36dfdcacefc67e222267c396978f6e6
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-28 15:51:49 +0900

    Query not queued users

--- batch/scheduler.go
+++ batch/scheduler.go
@@ -1,10 +1,60 @@
 package main
 
+import (
+	"log"
+	"time"
+)
+
+const (
+	batchSize = 100
+)
+
 func schedulerLoop(queue chan []int) {
-	for i := 0; i < 20; i++ {
-		queue <- []int{i}
+	for {
+		log.Printf("Len: %d\n", 100)
+		ids := notQueuedIds()
+
+		for _, id := range ids {
+			queue <- []int{id}
+		}
+
+		break
+		time.Sleep(1 * time.Minute)
 	}
 
+	var oldLen int
+	var newLen int
 	for {
+		oldLen = newLen
+		newLen = len(queue)
+
+		if oldLen != newLen {
+			log.Printf("[master] Queue: %d\n", newLen)
+		}
+		time.Sleep(1 * time.Second)
 	}
+}
+
+func notQueuedIds() []int {
+	ids := []int{}
+
+	rows, err := db.Query(
+		"SELECT id FROM users WHERE queued_at IS NULL LIMIT ?;",
+		batchSize,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var id int
+	for rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ids = append(ids, id)
+	}
+
+	return ids
 }

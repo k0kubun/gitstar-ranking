692c86784ce8c7fa2e8086dcb1d53acca9fea924
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-28 13:25:13 +0900

    Simple sql test

--- batch/main.go
+++ batch/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
-	println("Hello world")
+	db, err := sql.Open("mysql", "root:@/github_ranks_development")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id FROM users LIMIT 5;")
+	if err != nil {
+		panic(err)
+	}
+
+	var id int
+	for rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(id)
+	}
 }

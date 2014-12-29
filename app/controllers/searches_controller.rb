2f67332e390d78637de227175d9d5aa2986a9405
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-30 03:28:42 +0900

    Create valid search filed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def show
     search = User.search do
-      fulltext params[:keyword]
+      fulltext params[:q]
       paginate page: 1, per_page: 10
     end
     @users = search.results

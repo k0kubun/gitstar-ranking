0a1f9821843ebe8aa6e0932cbeae2efee46693d1
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-29 16:03:51 +0900

    Create simple toppage

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,4 @@
+class TopController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'top#index'
 
   resources :users, only: [] do
     collection do

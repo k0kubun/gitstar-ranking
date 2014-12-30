9a96635c6b8f542967ba98ce6329c7698b852024
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-30 20:33:53 +0900

    Implement organization ranking

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,7 @@
 class OrganizationsController < ApplicationController
+  PER_PAGE = 100
+
   def index
+    @organizations = Organization.starred_first.first(PER_PAGE)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   include Concerns::Starrable
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, omniauth_providers: [:github]
 
   validates :login, presence: true

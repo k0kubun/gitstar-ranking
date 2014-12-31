de4410941d35dd51d80411256b49be5f76390d3d
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-31 13:34:17 +0900

    Add new_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  # devise authentication fail redirection
+  def new_session_path(resource)
+    root_path
+  end
 end

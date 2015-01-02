7153c6b01763720de1428f362f62136d4cc0b730
Takashi Kokubun <takashikkbn@gmail.com>
2015-01-02 21:49:50 +0900

    Re drop obsolete files

--- app/jobs/update_repo_job.rb
+++
@@ -1,7 +0,0 @@
-class UpdateRepoJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(user_id)
-    # pending. old import job is removed and obsolete.
-  end
-end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,5 @@ set :stage, :production
 set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
-set :sidekiq_role, :app
 
 server ENV['DEPLOY_SSH_HOST'], user: ENV['DEPLOY_SSH_USER'], roles: %w[web app db]

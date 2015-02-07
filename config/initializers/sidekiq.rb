8c3c1e721bc84b67a102653f5a6150b0fbd93499
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-07 23:46:55 +0900

    Fix sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,1 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'githubranks-sidekiq' }
-end
+Sidekiq.redis = { url: 'redis://localhost:6379', namespace: 'githubranks-sidekiq' }

6b9f4f8a584b6336b2ed4b75a1ddbf69c283be19
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-08 02:48:17 +0900

    Use sidekiq as exception notification backend

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,5 @@
 require 'exception_notification/rails'
+require 'exception_notification/sidekiq'
 
 ExceptionNotification.configure do |config|
   # Ignore additional exception types.
@@ -15,28 +16,28 @@ ExceptionNotification.configure do |config|
 
   # Email notifier sends notifications by email.
   config.add_notifier :email, {
-    :email_prefix         => "[GitHub Ranks Exception] ",
-    :sender_address       => %["GitHub Ranks" <takashikkbn@gmail.com>],
-    :exception_recipients => %w[takashikkbn@gmail.com]
+    email_prefix: "[ERROR] ",
+    sender_address: %["GitHub Ranks" <takashikkbn@gmail.com>],
+    exception_recipients: %w[takashikkbn@gmail.com]
   }
 
   # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
   # config.add_notifier :campfire, {
-  #   :subdomain => 'my_subdomain',
-  #   :token => 'my_token',
-  #   :room_name => 'my_room'
+  #   subdomain: 'my_subdomain',
+  #   token: 'my_token',
+  #   room_name: 'my_room'
   # }
 
   # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
   # config.add_notifier :hipchat, {
-  #   :api_token => 'my_token',
-  #   :room_name => 'my_room'
+  #   api_token: 'my_token',
+  #   room_name: 'my_room'
   # }
 
   # Webhook notifier sends notifications over HTTP protocol. Requires 'httparty' gem.
   # config.add_notifier :webhook, {
-  #   :url => 'http://example.com:5555/hubot/path',
-  #   :http_method => :post
+  #   url: 'http://example.com:5555/hubot/path',
+  #   http_method: :post
   # }
 
 end

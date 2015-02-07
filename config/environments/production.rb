c0315736363071dcc0d5b51bbb0075ff1c8bce3e
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-07 20:49:10 +0900

    Introduce exception_notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    enable_starttls_auto: false,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

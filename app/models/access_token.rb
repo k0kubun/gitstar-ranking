41827b8d881ea5c90994436eeac4fc5cc81882dd
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-08 09:59:31 +0900

    Show rate limit

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,5 +1,14 @@
 class AccessToken < ActiveRecord::Base
+  CACHE_KEY = 'access_token-rate_limit'
+
   paginates_per 50
 
   belongs_to :user
+
+  def rate_limit
+    Rails.cache.fetch("#{CACHE_KEY}-#{token}", expires_in: 1.hour) do
+      rl = Octokit::Client.new(access_token: token).rate_limit
+      { remaining: rl.remaining, limit: rl.limit }
+    end
+  end
 end

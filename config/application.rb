33b85bf9a0ab7b36408504d5e113b140a085df71
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-10 23:11:48 +0900

    Change the application name

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GithubRanks
+module GithubRanking
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

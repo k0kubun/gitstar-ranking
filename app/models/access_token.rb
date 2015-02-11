5e1a231a4bd5bbf2084b5140a2a95678bf979ef8
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-11 15:25:13 +0900

    Implement more efficient token fetcher

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,41 +1,35 @@
 class AccessToken < ActiveRecord::Base
-  CACHE_KEY   = 'access_token-rate_limit'
+  TOKEN_LIST_KEY = 'access-token-list'
   TOKEN_LIMIT = 100
 
   paginates_per 50
 
   belongs_to :user
 
-  def self.fetch_rate_limit(token)
-    Rails.cache.delete("#{CACHE_KEY}-#{token}")
-    self.new(token: token).rate_limit
-  end
-
   def self.fetch_client
-    token = self.limit(TOKEN_LIMIT).pluck(:token).sample
-    Octokit::Client.new(access_token: token)
+    Octokit::Client.new(access_token: fetch_token)
   end
 
   def client
     Octokit::Client.new(access_token: token)
   end
 
-  def rate_limit
-    Rails.cache.fetch("#{CACHE_KEY}-#{token}", expires_in: 1.hour) do
-      rate_limit_without_cache
-    end
-  end
-
-  def rate_limit_without_cache
-    if authorized?
-      rate = client.rate_limit
-      { remaining: rate.remaining, limit: rate.limit }
-    else
-      { remaining: 0, limit: 0 }
-    end
-  end
-
   private
+
+  def self.fetch_token
+    token = Redis.current.rpoplpush(TOKEN_LIST_KEY, TOKEN_LIST_KEY)
+    return token if token.present?
+
+    prepare_token_list
+    Redis.current.rpoplpush(TOKEN_LIST_KEY, TOKEN_LIST_KEY)
+  end
+
+  def self.prepare_token_list
+    tokens = self.limit(TOKEN_LIMIT).pluck(:token)
+    tokens.each do |token|
+      Redis.current.rpush(TOKEN_LIST_KEY, token)
+    end
+  end
 
   def authorized?
     client.rate_limit.present?

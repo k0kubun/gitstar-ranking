b1dfd85451d69e8f85db2fe3fb041bebb2faafe5
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-12 10:49:56 +0900

    Join util and logger

--- batches/update_users/util.go
+++ batches/update_users/util.go
@@ -9,6 +9,23 @@ import (
 	"time"
 )
 
+var (
+	logF *os.File
+)
+
+func init() {
+	logF, err := os.OpenFile(
+		getEnv("GR_BATCH_LOG", "run.log"),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(logF)
+}
+
 func assert(err error) {
 	if err != nil {
 		log.Fatal(err)

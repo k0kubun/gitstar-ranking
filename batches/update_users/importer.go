99cf767cccee9012baa304ab1d8c33c0901d4543
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-12 21:19:43 +0900

    Bulk insert users

--- batches/update_users/importer.go
+++ batches/update_users/importer.go
@@ -4,10 +4,17 @@ import (
 	"github.com/octokit/go-octokit/octokit"
 )
 
+const (
+	importBatchSize = 200
+)
+
 func importWorker(impq chan *octokit.User) {
 	for {
-		user := <-impq
-		importUser(user)
+		users := []*octokit.User{}
+		for i := 0; i < importBatchSize; i++ {
+			users = append(users, <-impq)
+		}
+		importUsers(users)
 	}
 }
 
@@ -22,5 +29,29 @@ func importUser(user *octokit.User) {
 		user.PublicRepos,
 		user.ID,
 	)
+	logError(err)
+}
+
+func importUsers(users []*octokit.User) {
+	if len(users) == 0 {
+		return
+	}
+
+	sql := "INSERT INTO users (id, login, avatar_url, public_repos) VALUES "
+	values := []interface{}{}
+	for _, user := range users {
+		sql += "(?,?,?,?),"
+		values = append(values, user.ID, user.Login, user.AvatarURL, user.PublicRepos)
+	}
+	sql = sql[0 : len(sql)-1] // trim last ,
+	sql += `
+		ON DUPLICATE KEY UPDATE
+		id=VALUES(id),
+		login=VALUES(login),
+		avatar_url=VALUES(avatar_url),
+		public_repos=VALUES(public_repos);
+	`
+
+	_, err := db.Exec(sql, values...)
 	logError(err)
 }

a94e826b471655fde025295da904f345e914e97b
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-12 22:32:10 +0900

    Add link tag to pageable actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,12 @@
 module ApplicationHelper
+  include PaginationHelper
+
   PAGE_TITLE  = 'GitHub Ranking'
   GITHUB_HOST = 'https://github.com'
   COLOR_CLASS = {
     notice: :success,
     alert: :danger,
   }
-
-  # Large offset causes slow query.
-  # FIXME: maybe kaminari has this kind of option
-  def limited_paginate(scope, options = {}, &block)
-    paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(
-      current_page: scope.current_page,
-      per_page:     scope.limit_value,
-      total_count:  1000,
-      total_pages:  pagination_limit,
-      remote:       false,
-    )
-    paginator.to_s
-  end
-
-  def sunspot_paginate(results, options = {}, &block)
-    paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(
-      current_page: results.current_page,
-      per_page:     results.limit_value,
-      total_count:  results.total_count,
-      total_pages:  [results.total_pages, pagination_limit].min,
-      remote:       false,
-    )
-    paginator.to_s
-  end
 
   def link_to_github(path, options = {}, &block)
     github_url = File.join(GITHUB_HOST, path)

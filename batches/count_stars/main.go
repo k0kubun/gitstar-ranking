6c822fd38a5cdbd014d253004426606c94a88a8a
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-13 21:00:25 +0900

    Copy update_users as count_stars

---
+++ batches/count_stars/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/octokit/go-octokit/octokit"
+	"runtime"
+)
+
+const (
+	requestConcurrency = 100
+	bufferedImports    = 1000
+	bufferedRequests   = 1000
+	bufferedDestroys   = 1000
+)
+
+func main() {
+	defer db.Close()
+	defer logF.Close()
+	runtime.GOMAXPROCS(requestConcurrency + 3)
+
+	updateAllUsers()
+}
+
+func updateAllUsers() {
+	importQueue := make(chan *octokit.User, bufferedImports)
+	requestQueue := make(chan int, bufferedRequests)
+	destroyQueue := make(chan int, bufferedDestroys)
+
+	go importWorker(importQueue)
+	go destroyWorker(destroyQueue)
+	for i := 0; i < requestConcurrency; i++ {
+		go requestWorker(requestQueue, importQueue, destroyQueue)
+	}
+
+	scheduleAll(requestQueue)
+}

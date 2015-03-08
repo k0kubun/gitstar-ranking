f5cee54b53add8c28672e5f4a71e24e0a337f99c
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-08 20:31:09 +0900

    Stop using worker

--- lib/github/location_initializer.rb
+++ lib/github/location_initializer.rb
@@ -6,15 +6,9 @@ module Github
     CONCURRENCY = 4
 
     def run
-      worker_pool = Worker.new CONCURRENCY, lambda { |user_id, worker_index|
-        update_location(user_id)
-      }
-
-      iterate_all(User.all, [:id], 0, false) do |id|
-        worker_pool.enq(id)
+      iterate_all(User.all, [:id], 0, true) do |id|
+        update_location(id)
       end
-
-      worker_pool.stop
     end
 
     private
@@ -23,7 +17,6 @@ module Github
       client = AccessToken.fetch_client
       location = client.user(id)[:location]
       User.where(id: id).update_all(location: location)
-      print "#{id}\r"
     rescue => e
       logger.error "#{e.class} (#{user_id}): #{e}"
     end

534a03258cb898b0fcf24de3cf1bd4a0d3bfc772
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-22 10:01:56 +0900

    Specify rbenv_ruby for capistrano-rbenv

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,8 @@ set :rails_env, 'production'
 set :migration_role, 'db'
 set :sidekiq_role, 'app'
 
+set :rbenv_ruby, File.read(File.expand_path('../../.ruby-version', __dir__)).strip
+
 set :user, 'k0kubun'
 role :app, 'gmo'
 role :web, 'gmo'

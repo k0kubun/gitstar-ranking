048458f878401c4e1dccce19a7a8df1b3a3671ef
TaihakuTsuchiya <tofo.ri.haku.kyoi@gmail.com>
2015-11-01 13:26:12 +0900

    Resolves #12 - Usage: LOGIN=name user:create

---
+++ lib/github/user_generator.rb
@@ -0,0 +1,21 @@
+module Github
+  class UserGenerator
+    FETCH_ATTRIBUTES = %i[id login avatar_url type]
+
+    def run(login)
+      user = User.new(fetch_attributes_of(login))
+      user.save!
+      user
+    end
+
+    private
+
+    def fetch_attributes_of(login)
+      user = Octokit.user(login)
+
+      user.each_with_object({}) do |(key, value), user_attributes|
+        user_attributes[key] = v if FETCH_ATTRIBUTES.include?(key)
+      end
+    end
+  end
+end

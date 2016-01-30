1e0e129aafb04f7b571740bb544ce6936eacecdc
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-30 19:48:59 +0900

    Migrate app server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,13 +2,14 @@ set :stage, :production
 set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
-set :sidekiq_role, 'app'
+set :sidekiq_role, 'sidekiq'
 
 set :rbenv_ruby, File.read(File.expand_path('../../.ruby-version', __dir__)).strip
 
 set :user, 'k0kubun'
-role :app, 'gmo'
-role :web, 'gmo'
-role :db,  'excloud'
+role :app, 'cloudcore'
+role :web, 'cloudcore'
+role :db,  []
+role :sidekiq, []
 
 set :deploy_to, '/home/k0kubun/github-ranking'

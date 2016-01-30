d3786c6eed33a2a419c291c840824de255c124f1
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-31 00:30:05 +0900

    Deploy sidekiq

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@ set :sidekiq_role, 'sidekiq'
 set :rbenv_ruby, File.read(File.expand_path('../../.ruby-version', __dir__)).strip
 
 set :user, 'k0kubun'
-role :app, 'cloudcore'
-role :web, 'cloudcore'
+role :app, %w[cloudcore]
+role :web, %w[cloudcore]
 role :db,  []
-role :sidekiq, []
+role :sidekiq, %w[cloudcore]
 
 set :deploy_to, '/home/k0kubun/github-ranking'

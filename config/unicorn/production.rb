c406ac064e8e3c63a73a8cf0a044d1e9cf79aae9
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-31 00:40:20 +0900

    Reduce workers

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -14,7 +14,7 @@ stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
 
 # workers
-worker_processes 4
+worker_processes 3
 
 # use correct Gemfile on restarts
 before_exec do |server|

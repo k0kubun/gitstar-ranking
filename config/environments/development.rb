2f6e7d21cd97e611446d71c4b6af7fdc4ec3b7a8
Takashi Kokubun <takashikkbn@gmail.com>
2016-01-31 11:10:03 +0900

    Resurrect custom configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Custom configs
+  config.middleware.swap Rails::Rack::Logger, Silencer::Logger, silence: [%r{\A/assets/}]
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Custom configs
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    enable_starttls_auto: false,
+  }
 end

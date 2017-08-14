3ff6d7f89a117cb735c2783fbff719c528c37639
Takashi Kokubun <takashikkbn@gmail.com>
2017-08-13 23:44:08 -0700

    Add missing type annotation comment

--- app/models/update_user_job.rb
+++ app/models/update_user_job.rb
@@ -1,6 +1,7 @@
 class UpdateUserJob < ActiveRecord::Base
   # Kick UpdateUserWorker.java via MySQL queue
   # @param [Integer] user_id
+  # @param [Integer] token_user_id
   def self.perform_later(user_id:, token_user_id:)
     # Payload is parsed in: worker/src/main/java/com/github/k0kubun/github_ranking/dao/UpdateUserJobDao.java
     payload = {

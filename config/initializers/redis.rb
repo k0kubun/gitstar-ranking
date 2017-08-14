a81a577ad264e6d06b5fd0953be5671fe426b682
Takashi Kokubun <takashikkbn@gmail.com>
2017-08-13 23:41:47 -0700

    Sync back settings from production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-redis_url = 'redis://localhost:6379'
+host = 'localhost'
+port = 6379
+redis_url = "redis://#{host}:#{port}"
+
+Redis.current = Redis.new(host: host, port: port)
 Rails.application.config.cache_store = :redis_store, "#{redis_url}/0/cache", { expires_in: 90.minutes }

53ae34a22caa8d2efa8bd3342e1544152c22656b
Takashi Kokubun <takashikkbn@gmail.com>
2017-08-28 14:35:41 +0900

    Add #graphql_id method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # For CLI usage, not used by app
+  def graphql_id
+    Base64.encode64("04:#{self.class.name}#{id}").rstrip
+  end
 end

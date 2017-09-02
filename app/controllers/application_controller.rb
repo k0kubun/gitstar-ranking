3c79ddf1d353d9e818df23484769024d9ac60813
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-02 14:17:00 +0000

    Configure sentry-raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_raven_context
+
   private
 
   # devise authentication fail redirection
   def new_session_path(resource)
     root_path
+  end
+
+  def set_raven_context
+    if current_user
+      Raven.user_context(login: current_user.login)
+    end
   end
 end

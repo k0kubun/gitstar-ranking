e44e3caa7faba9d37b9489155d78a591ad0f1297
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-10 15:12:59 +0900

    Publish and link to graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-
-  post "/graphql", to: "graphql#execute"
   root to: 'top#index'
+
+  post '/graphql', to: 'graphql#execute'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 
   authenticate :user, ->(u) { u.admin? } do
     resources :access_tokens, only: :index do

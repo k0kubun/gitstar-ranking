4e2cf2f77011e3968a1e5bdf09bb81fa2f4530b9
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-10 20:17:55 +0900

    Pass label and path from data attribute

--- app/javascript/components/update_button.js
+++ app/javascript/components/update_button.js
@@ -1,14 +1,19 @@
 import React, { PropTypes } from 'react';
 
 export default class UpdateButton extends React.Component {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired
+  };
+
   constructor(props) {
     super(props);
   }
 
   render() {
     return React.createElement('a',
-      {className: 'btn btn-info col-xs-12', href: '/update_myself', method: 'post'},
-      'Update your stars'
+      {className: 'btn btn-info col-xs-12', href: this.props.path, method: 'post'},
+      this.props.label
     );
   }
 }
--- app/javascript/packs/users/show.js
+++ app/javascript/packs/users/show.js
@@ -5,8 +5,10 @@ import UpdateButton from '../../components/update_button';
 document.addEventListener('DOMContentLoaded', function() {
   var mountNode = document.getElementById('update_button');
   if (mountNode != null) {
+    var path = mountNode.getAttribute('data-path');
+    var label = mountNode.getAttribute('data-label');
     ReactDOM.render(
-      React.createElement(UpdateButton, null, null),
+      React.createElement(UpdateButton, {path: path, label: label}, null),
       mountNode
     );
   }

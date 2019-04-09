165c67d60d3cb85b051fe1c324a9492be8a289c8
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-09 23:33:30 +0900

    Issue UpdateUserWorker for login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   include Concerns::Pageable
 
+  GITHUB_HOST = 'https://github.com'
   PER_PAGE = 50
 
   before_action :validate_page_param, only: :index
@@ -13,6 +14,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(login: params[:login])
     @repositories = @user.repositories.page(params[:page]).per(PER_PAGE)
+  rescue ActiveRecord::RecordNotFound
+    user_name = request.path.sub(/\A\//, '')
+    if current_user && user_exists_on_github?(user_name)
+      UpdateUserJob.perform_later(token_user_id: current_user.id, user_name: user_name)
+      redirect_to root_path, notice: "Requested to create a page for #{user_name.dump}. Wait for a while and check it later."
+    else
+      raise
+    end
   end
 
   def update_org
@@ -43,5 +52,12 @@ class UsersController < ApplicationController
     end
 
     redirect_to current_user, notice: 'Update request is successfully queued. Please wait a moment.'
+  end
+
+  private
+
+  def user_exists_on_github?(user_name)
+    conn = Faraday.new(url: GITHUB_HOST)
+    conn.get("/#{user_name}").status == 200
   end
 end

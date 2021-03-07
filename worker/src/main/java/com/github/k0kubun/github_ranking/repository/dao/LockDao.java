90d173e9ffdff1712478e510161e6e0bf20524c1
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-06 21:21:29 -0800

    It's SqlUpdate

--- worker/src/main/java/com/github/k0kubun/github_ranking/repository/dao/LockDao.java
+++ worker/src/main/java/com/github/k0kubun/github_ranking/repository/dao/LockDao.java
@@ -1,10 +1,10 @@
 package com.github.k0kubun.github_ranking.repository.dao;
 
 import org.skife.jdbi.v2.sqlobject.Bind;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface LockDao
 {
-    @SqlQuery("select pg_advisory_xact_lock(:key)")
+    @SqlUpdate("select pg_advisory_xact_lock(:key)")
     void getLock(@Bind("key") long key);
 }

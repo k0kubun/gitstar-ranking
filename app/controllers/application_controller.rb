d1a2f5798a419dfaa1310d66d04f1122351e5422
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-06 20:18:30 -0800

    Revert "Workaround"
    This reverts commit 76e513434930fe4ce8455d9d9e5d3402641f51a1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :set_raven_context
+  before_action :set_raven_context
 
   private
 

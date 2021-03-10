897e20a492427d17997f86ab704147f06a40c835
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-10 02:28:23 -0800

    Just skip the transition for now

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  if (!document.querySelector('.users_controller.show_action'))
+    return;
+
+  const userUpdate = document.getElementById('user-update');
+  if (userUpdate) {
+    userUpdate.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      userUpdate.innerHTML = 'Updating your stars...';
+      userUpdate.classList.add('disabled');
+      userUpdate.classList.remove('btn-info');
+      userUpdate.classList.add('btn-default');
+    });
+  }
+});

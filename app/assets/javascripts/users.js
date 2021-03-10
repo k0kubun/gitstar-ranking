b897df7beae96534cd80d945f8120fc4271e63ce
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-10 02:33:10 -0800

    Revert "Fix the return"
    This reverts commit 2e260ee02f5caa57c18ed210459e8054dbc478d4.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,14 @@
-if (document.querySelector('.users_controller.show_action')) {
-  const userUpdate = document.getElementById('user-update');
+if (!document.querySelector('.users_controller.show_action'))
+  return;
 
-  if (userUpdate) {
-    userUpdate.addEventListener('click', (event) => {
-      event.preventDefault();
+const userUpdate = document.getElementById('user-update');
+if (userUpdate) {
+  userUpdate.addEventListener('click', (event) => {
+    event.preventDefault();
 
-      userUpdate.innerHTML = 'Updating your stars...';
-      userUpdate.classList.add('disabled');
-      userUpdate.classList.remove('btn-info');
-      userUpdate.classList.add('btn-default');
-    });
-  }
+    userUpdate.innerHTML = 'Updating your stars...';
+    userUpdate.classList.add('disabled');
+    userUpdate.classList.remove('btn-info');
+    userUpdate.classList.add('btn-default');
+  });
 }

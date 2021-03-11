ca1ecf4ffd8df93ede673896c82e0b160bd7ef16
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-11 00:36:39 -0800

    Fix deprecation warnings in the Gradle config

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 application {
-    mainClassName = "com.github.k0kubun.gitstar_ranking.Main"
+    mainClass.set("com.github.k0kubun.gitstar_ranking.Main")
 }
 
 repositories {
@@ -12,16 +12,16 @@ repositories {
 }
 
 dependencies {
-    compile("ch.qos.logback:logback-classic:1.3.0-alpha4")
-    compile("com.google.guava:guava:21.0")
-    compile("com.google.http-client:google-http-client:1.22.0")
-    compile("io.sentry:sentry:1.5.2")
-    compile("javax.json:javax.json-api:1.1")
-    compile("javax.ws.rs:javax.ws.rs-api:2.0.1")
-    compile("org.postgresql:postgresql:42.2.19")
-    compile("org.antlr:stringtemplate:3.2.1") // Using ST3 because ST4 isn"t supported by JDBI 2
-    compile("org.glassfish:javax.json:1.1")
-    compile("org.jboss.resteasy:resteasy-jackson2-provider:3.1.2.Final")
-    compile("org.jdbi:jdbi:2.78")
-    testCompile("junit:junit:4.12")
+    implementation("ch.qos.logback:logback-classic:1.3.0-alpha4")
+    implementation("com.google.guava:guava:21.0")
+    implementation("com.google.http-client:google-http-client:1.22.0")
+    implementation("io.sentry:sentry:1.5.2")
+    implementation("javax.json:javax.json-api:1.1")
+    implementation("javax.ws.rs:javax.ws.rs-api:2.0.1")
+    implementation("org.postgresql:postgresql:42.2.19")
+    implementation("org.antlr:stringtemplate:3.2.1") // Using ST3 because ST4 isn"t supported by JDBI 2
+    implementation("org.glassfish:javax.json:1.1")
+    implementation("org.jboss.resteasy:resteasy-jackson2-provider:3.1.2.Final")
+    implementation("org.jdbi:jdbi:2.78")
+    testImplementation("junit:junit:4.12")
 }

9197ae5fa37cfdfe24f6452c945c10c8627e7bda
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-10 20:34:51 -0800

    Cosmetic changes in specs

--- spec/request/repositories_spec.rb
+++ spec/request/repositories_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe 'repositories', type: :request do
         :repository,
         name: 'animate.css',
         full_name: 'daneden/animate.css',
-        owner: FactoryGirl.create(:user, login: 'daneden')
+        owner: FactoryGirl.create(:user, login: 'daneden'),
       )
       get "/#{repo.full_name}"
       expect(response.status).to eq(200)
       expect(response.headers['Content-Type']).to match(%r[\Atext/html;])
     end
 
-    it 'renders .jss path with proper Content-Type' do
+    it 'renders .js path with proper Content-Type' do
       repo = FactoryGirl.create(
         :repository,
         name: 'angular.js',
         full_name: 'angular/angular.js',
-        owner: FactoryGirl.create(:user, login: 'angular')
+        owner: FactoryGirl.create(:user, login: 'angular'),
       )
       get "/#{repo.full_name}"
       expect(response.status).to eq(200)

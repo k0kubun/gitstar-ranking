221bddf3019424ad0fa315ef999c47b2fad6bfc4
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-10 22:04:12 -0800

    Fix keyward args

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -2,13 +2,13 @@ module PaginationHelper
   # Large offset causes slow query.
   # FIXME: maybe kaminari has this kind of option
   def limited_paginate(scope, options = {}, &block)
-    paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(
+    paginator = Kaminari::Helpers::Paginator.new(self, **options.reverse_merge(
       current_page: scope.current_page,
       per_page:     scope.limit_value,
       total_count:  1000,
       total_pages:  pagination_limit,
       remote:       false,
-    )
+    ))
     paginator.to_s
   end
 end

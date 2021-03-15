9341b773cbeed6ca69e33bc64ea962f04f07abea
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-15 02:24:48 -0700

    Install JavaTimeModule
    to approach:
    
    org.jooq.exception.DataTypeException: No Converter found for types java.lang.Object and java.sql.Timestamp

--- worker/src/main/kotlin/com/github/k0kubun/gitstar_ranking/core/ObjectMapper.kt
+++ worker/src/main/kotlin/com/github/k0kubun/gitstar_ranking/core/ObjectMapper.kt
@@ -1,10 +1,13 @@
 package com.github.k0kubun.gitstar_ranking.core
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
-val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
+val objectMapper: ObjectMapper = ObjectMapper()
+    .registerModule(KotlinModule())
+    .registerModule(JavaTimeModule())
     .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)

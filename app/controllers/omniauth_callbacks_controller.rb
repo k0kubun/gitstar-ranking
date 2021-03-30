8a281d008b11aa504f3047b3b5093ab211b280ce
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-30 10:48:11 -0700

    Deal with user conflict
    Fix #80

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,11 +8,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     image = auth[:info][:image]
     token = auth[:credentials][:token]
 
-    @user = User.where(id: id).first_or_create(
-      login:      login,
-      type:       'User',
-      avatar_url: image,
-    )
+    @user = without_user_conflict(id: id, login: login) do 
+      User.where(id: id).first_or_create(
+        login:      login,
+        type:       'User',
+        avatar_url: image,
+      )
+    end
     register_token(id, token)
 
     sign_in @user
@@ -20,6 +22,21 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
+
+  def without_user_conflict(id:, login:, &block)
+    # Delete a conflicting user who changed its login
+    user = User.find_by(login: login)
+    if user.id != id
+      user.destroy
+    end
+
+    result = block.call
+
+    if user.id != id
+      UserUpdateJob.perform_later(user_id: user.id, token_user_id: id)
+    end
+    result
+  end
 
   def register_token(user_id, token)
     access_token = AccessToken.where(user_id: user_id).first_or_create(token: token)
